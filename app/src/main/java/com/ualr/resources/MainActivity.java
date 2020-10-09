a6df05b7ab1f217f9ff941c0c76c8de4d1bfea0e
pmplouch <42617910+pmplouch@users.noreply.github.com>
2020-10-09 17:07:54 -0500

    Submission

--- app/src/main/java/com/ualr/resources/MainActivity.java
+++ app/src/main/java/com/ualr/resources/MainActivity.java
@@ -4,9 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
 
 // TODO 3: Define the alternative resources needed to have different content and look n feel depending on the device language.
 // TODO 4: Get the description string value from resources
@@ -18,6 +26,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView country_desc = findViewById(R.id.country_description_text);
+        try {
+            loadToTextView(country_desc);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadToTextView(TextView textView) throws Exception
+    {
+        InputStream inputStream;
+
+        if(getString(R.string.country_short).equals("usa"))
+        {
+            inputStream = getResources().openRawResource(R.raw.usa_string);
+        }
+        else
+        {
+            inputStream = getResources().openRawResource(R.raw.spain_string);
+        }
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        int i;
+        try {
+            i = inputStream.read();
+            while (i != -1)
+            {
+                byteArrayOutputStream.write(i);
+                i = inputStream.read();
+            }
+            inputStream.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        textView.setText(byteArrayOutputStream.toString());
     }
 
 }
